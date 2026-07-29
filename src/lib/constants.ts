c5cafa86c6162faf0dec4fa554c20fb943b1eb33
unknown <thienan204@gmail.com>
2026-07-29 11:52:35 +0700

    feat: Bổ sung thêm các đường dẫn vào Không gian Quản trị (ADMIN_ONLY_PATHS)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,17 @@
 // File chứa các hằng số hệ thống dùng chung cho toàn bộ dự án
 
 export const ADMIN_ONLY_PATHS = [
-    '/rules', 
-    '/roles', 
-    '/settings', 
-    '/admin', 
-    '/mau', 
-    '/tt32', 
-    '/api-management', 
-    '/schema', 
-    '/chuyen-de/quy-tac-chuyen-de'
+    '/rules',
+    '/roles',
+    '/settings',
+    '/admin',
+    '/mau',
+    '/tt32',
+    '/api-management',
+    '/schema',
+    '/chuyen-de/quy-tac-chuyen-de',
+    '/excel-rules',
+    '/docs',
+    '/users',
+    '/error-management/duty-roster'
 ];
